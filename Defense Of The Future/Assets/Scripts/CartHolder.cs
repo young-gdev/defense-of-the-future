98b39d15dd8dcb785f3bbb2d1ed78a95b7307ad7
young-gdev <uliana2015levschina@gmail.com>
2022-05-03 14:55:06 +0300

    ОБНОВА!
    Крч теперь можно карточки добавлять со своей ценой, иконкой и тд и тп

--- Defense Of The Future/Assets/Scripts/CartHolder.cs
+++ Defense Of The Future/Assets/Scripts/CartHolder.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CartHolder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Card Holder Parameters")]
+    [SerializeField] private Transform _cardHolderPosition;
+    [SerializeField] private GameObject _card;
+    [SerializeField] private Card[] _cardSO;
+    private int _cardsAmount;
+
+    [Header("Card Parameters")]
+    [SerializeField] private GameObject[] _plantedCards;
+    private int _cost;
+    private Sprite _icon;
+
+    private void Start()
     {
-        
+        _cardsAmount = _cardSO.Length;
+        _plantedCards = new GameObject[_cardsAmount];
+
+        for (int i = 0; i < _cardsAmount; i++)
+        {
+            CreateCard(i);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateCard(int i)
     {
-        
+        var card = Instantiate(_card, _cardHolderPosition);
+
+        _plantedCards[i] = card;
+
+        _icon = _cardSO[i].icon;
+        _cost = _cardSO[i].cost;
+
+        card.GetComponentInChildren<SpriteRenderer>().sprite = _icon;
+        card.GetComponentInChildren<TMP_Text>().text = _cost.ToString();
     }
 }
