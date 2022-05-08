9c6b0d2305f264cc024a5065331d6892e85e82c9
Unventure <unventurestudio@gmail.com>
2022-05-08 23:39:13 +0300

    mine(3;2) and (2;2)

--- Defense Of The Future/Assets/Scripts/BuildMode.cs
+++ Defense Of The Future/Assets/Scripts/BuildMode.cs
@@ -6,12 +6,21 @@ using UnityEngine.UI;
 public class BuildMode : MonoBehaviour
 {
     public bool buildmode = false;
+
     public string unit = "none";
-    [SerializeField] GameObject mine;
-    public ParticleSystem spawnEffect;
-    public bool blockSpawn = false;
-    public ParticleSystem BlockedEffect;
-    
+
+    [SerializeField] GameObject mine2x2;
+    [SerializeField] GameObject mine3x2;
+
+    public ParticleSystem spawnEffect2x2;
+    public ParticleSystem spawnEffect3x2;
+
+    public bool blockSpawn2x2 = false;
+    public bool blockSpawn3x2 = false;
+
+    public ParticleSystem BlockedEffect2x2;
+    public ParticleSystem BlockedEffect3x2;
+
 
 
     public void MineSpawn()
@@ -19,28 +28,50 @@ public class BuildMode : MonoBehaviour
         buildmode = true;
         unit = "mine";
     }
-    
-    public void SpawnAnyObject()
+
+    public void Button2x2()
     {
         if (buildmode == true)
         {
-            if (blockSpawn == false)
+            if (blockSpawn2x2 == false)
             {
                 if (unit == "mine")
                 {
-                    Instantiate(mine);
-                    spawnEffect.Play();
-                    blockSpawn = true;
+                    Instantiate(mine2x2);
+                    
+                    spawnEffect2x2.Play();
+                    blockSpawn2x2 = true;
                     buildmode = false;
                 }
             }
             else
             {
-                BlockedEffect.Play();
+                BlockedEffect2x2.Play();
             }
         }
-        else
+        
+    }
+
+    public void Button3x2()
+    {
+        if (buildmode == true)
         {
+            if (blockSpawn3x2 == false)
+            {
+                if (unit == "mine")
+                {
+                    Instantiate(mine3x2);
+
+                    spawnEffect3x2.Play();
+                    blockSpawn3x2 = true;
+                    buildmode = false;
+                }
+            }
+            else
+            {
+                BlockedEffect3x2.Play();
+            }
         }
+
     }
 }
